0511a6c62d64483fbf1915ea4cfa183af4059afc
paperpatch <patchen21@gmail.com>
2021-12-07 16:01:18 -0500

    added mergesort animation to visualized. addded import CSS, secondary color and animation speed

--- src/SortingVisualizer/sortingVisualizer.jsx
+++ src/SortingVisualizer/sortingVisualizer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {getMergeSortAnimations} from '../SortingAlgorithms/mergeSort.js';
+import './SortingVisualizer.css';
 
 const NUMBER_OF_ARRY_BARS = 100;
+const ANIMATION_SPEED_MS = 1;
 
 const PRIMARY_COLOR = 'blue';
-// const SECONDARY_COLOR = 'red';
+const SECONDARY_COLOR = 'red';
 
 export default class SortingVisualizer extends React.Component {
   constructor(props) {
@@ -28,7 +30,28 @@ export default class SortingVisualizer extends React.Component {
   }
 
   mergeSort() {
-    const animations = getMergeSortAnimations()
+    const animations = getMergeSortAnimations(this.state.array);
+    for (let i=0; i<animations.length; i++) {
+      const arrayBars = document.getElementsByClassName('array-bar');
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIndex, barTwoIndex] = animations[i];
+        const barOneStyle = arrayBars[barOneIndex].style;
+        const barTwoStyle = arrayBars[barTwoIndex].style;
+        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * ANIMATION_SPEED_MS);
+      } else {
+        setTimeout(() => {
+          const [barOneIndex, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIndex].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * ANIMATION_SPEED_MS);
+      }
+    }
   }
 
   quickSort() {}
@@ -65,8 +88,6 @@ export default class SortingVisualizer extends React.Component {
     )
   }
 }
-
-
 
 // generate random number between 2 numbers in javascript
 function randomIntFromInterval(min, max) {
