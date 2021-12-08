50eff546f83f4223eae6fc6e2be8b35e1a86f2d1
paperpatch <patchen21@gmail.com>
2021-12-08 14:50:01 -0500

    added commented merge sort to understand the functions

--- src/SortingAlgorithms/mergeSort.js
+++ src/SortingAlgorithms/mergeSort.js
@@ -60,3 +60,35 @@ function doMerge(
     mainArray[k++] = auxiliaryArray[j++];
   }
 }
+
+// Understanding Merge Sort
+/* 
+
+const mergeSort = array => {
+  if (array.length === 1) return array;
+
+  const middleIdx = Math.floor(array.length / 2);
+  const firstHalf = mergeSort(array.slice(0, middleIdx));
+  const secondHalf = mergeSort(array.slice(middleIdx));
+  
+  const sortedArray = [];
+
+  let i = 0;
+  let j = 0;
+
+  // compare and check which number is bigger. push smaller number.
+  while (i < firstHalf.length && j < secondHalf.length) {
+    if (firstHalf[i] < secondHalf[j]) {
+      sortedArray.push(firstHalf[i++])
+    } else {
+      sortedArray.push(secondHalf[j++]);
+    }
+  }
+  // checks if all numbers in array were sorted. This happens when comparing odd number arrays. Ex: Compare [3, 5] and [4]
+  while (i < firstHalf.length) sortedArray.push(firstHalf[i++]);
+  while (j < secondHalf.length) sortedArray.push(secondHalf[j++]);
+
+  return sortedArray;
+}
+
+ */
