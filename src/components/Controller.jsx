c509be385be94cf4572876ca7ac956720251da4a
paperpatch <patchen21@gmail.com>
2021-12-17 18:09:15 -0500

    added Controller template

--- src/components/Controller.jsx
+++ src/components/Controller.jsx
@@ -20,3 +20,138 @@ import {
   getRandomArray,
 } from "../common/helper";
 
+const ControlBar = styled.div`
+  font-size: 2rem;
+  display: flex;
+  align-items: center;
+  margin: 15px 0;
+  flex-wrap: wrap;
+`;
+
+const ArrayBar = styled.div`
+  display: flex;
+  align-items: center;
+  flex-basis: 60%;
+  flex-grow: 1;
+  min-width: 300px;
+`;
+
+const ExecutionBar = styled.div`
+  display: flex;
+  align-items: center;
+  flex-basis: 40%;
+  flex-grow: 1;
+`;
+
+export function Controller() {
+  const [isPausing, setIsPausing] = useState(false);
+
+  const [progress, speed] = useControls(
+    (state) => [state.progress, state.speed],
+    shallow
+  );
+
+  const [sortingArray, setSortingArray] = useData(
+    (state) => [state.sortingArray, state.setSortingArray],
+    shallow
+  );
+
+  const [startSorting, pauseSorting, resetSorting, setSpeed] = useControls(
+    (state) => [
+      state.startSorting,
+      state.pauseSorting,
+      state.resetSorting,
+      state.setSpeed,
+    ],
+    shallow
+  );
+
+  const [arrayInput, setArrayInput] = useState(sortingArray);
+
+  const startElement = <VscDebugStart onClick={startSorting} />;
+  const pauseElement = <ImPause onClick={pauseAndDelaySorting} />;
+  const resetElement = <VscDebugRestart onClick={resetSorting} />;
+  const disabledPauseElement = <ImPause style={{ color: "#e5e5e5" }} />;
+
+  async function pauseAndDelaySorting() {
+    pauseSorting();
+    setIsPausing(true);
+    await delay(useControls.getState().swapTime);
+    setIsPausing(false);
+  }
+
+  function arrayDataChangeHandler(value) {
+    const arrayString = convertInputToArrayString(value);
+    setArrayInput(arrayString);
+
+    const array = convertArrayStringToArray(arrayString);
+    setSortingArray(array);
+    resetSorting();
+  }
+
+  function generate() {
+    const randomArray = getRandomArray();
+    setArrayInput(randomArray);
+    setSortingArray(randomArray);
+    resetSorting();
+  }
+
+  function getProgressButton() {
+    if(isPausing)
+      return disabledPauseElement;
+
+    // eslint-disable-next-line default-case
+    switch (progress) {
+      case "reset":
+        return startElement;
+      case "start":
+        return pauseElement;
+      case "pause":
+        return startElement;
+      case "done":
+        return disabledPauseElement;
+    }
+  }
+
+  return (
+    <ControlBar>
+      <ArrayBar>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick = {generate}
+          ></Button>
+
+          <TextField
+           id="outlined-basic"
+           label="Input"
+           variant="outlined"
+           onChange={(event) => arrayDataChangeHandler(event.target.value)}
+           value={arrayInput}
+           size="small"
+           width="100px"
+           style={{ flexGrow: 1, margin: '0 10px' }} 
+          />
+      </ArrayBar>
+      <ExecutionBar>
+        <Slider
+          key={`slider-${speed}`}
+          defaultValue={speed}
+          onChange={(event, value) => setSpeed(value)}
+          aria-labelledby="discrete-slider"
+          valueLabelDisplay="auto"
+          step={1}
+          marks
+          min={1}
+          max={10}
+          style={{ flexGrow: 1, flexBasis: "100%" }}
+        />
+
+        <div style={{ display: "flex", marginLeft: '20px', columnGap: '5px' }}>
+          {getProgressButton()}
+          {resetElement}
+        </div>
+      </ExecutionBar>
+    </ControlBar>
+  );
+}
