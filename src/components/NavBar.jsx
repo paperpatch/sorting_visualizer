f4d180dac5d1b592cfc335abe736f23197479a89
paperpatch <patchen21@gmail.com>
2021-12-18 11:16:03 -0500

    fixed backgroundColor compile error

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,7 +20,7 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     width: "100%",
-    backgroundColor: theme.palette.background.paper,
+    backgroundColor: theme.background,
   },
 }));
 
