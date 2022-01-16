5a92443c041439d7ef370f67bf725a45840f16ef
paperpatch <patchen21@gmail.com>
2022-01-16 14:56:34 -0500

    npm audit for vulnerable dependency

--- src/sortFunctions/bubbleSort.js
+++ src/sortFunctions/bubbleSort.js
@@ -12,3 +12,37 @@ export async function* bubbleSort(array, swap, highlight, marksort) {
     yield;
   }
 }
+
+/*
+// Understanding Bubble Sort
+
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      // compare arr[j] to arr[j+1]
+      // swap places if needed
+    }
+  }
+
+  return arr;
+};
+
+// Alternative Approach
+
+const bubbleSort = (arr) => {
+  let sorted = false;
+
+  while (!sorted) {
+    sorted = true;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+      // compare arr[i] to arr[i+1]
+      // swap places if needed
+      // if swapped, set sorted = false to run while loop again
+    }
+  }
+
+  return arr;
+};
+
+*/
